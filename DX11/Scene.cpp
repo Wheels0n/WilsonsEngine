d76033386ca130be49e4be38b1d2dfbf2e2cdd5e
JihoKim <wlgh0655@gmail.com>
2023-01-23 08:37:40 +0900

    엔티티가 TreeNode에서 leaf로 바뀜

--- DX11/Scene.cpp
+++ DX11/Scene.cpp
@@ -24,17 +24,18 @@ void CScene::Draw()
 {
 	if (ImGui::Begin("Scene Hierarchy"))
 	{   
+		ImGuiTreeNodeFlags node_flags;
 		if (ImGui::TreeNode(m_name.c_str()))
-		{   
+		     {   
 			ImGui::SetNextItemOpen(true, ImGuiCond_Once);
 			for (int i = 0; i < m_entites.size(); ++i)
 			{
 				std::string name = *(m_entites[i]->GetType());
 
-				if (ImGui::TreeNode((void*)(intptr_t)i, name.c_str(), i))
+				node_flags |= ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen;
+				if (ImGui::TreeNodeEx((void*)(intptr_t)i, node_flags, name.c_str(), i))
 				{
 					m_pSelectionETT = m_entites[i];
-					ImGui::TreePop();
 				}
 				
 			}
